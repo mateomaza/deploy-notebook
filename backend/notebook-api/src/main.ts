00cd52b652b8e70b3cdb1220471695c343f3b0d5
mateomaza <1mateomaza@gmail.com>
2024-05-16 15:58:01 -0500

    Could be

--- backend/notebook-api/src/main.ts
+++ backend/notebook-api/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   if (process.env.NODE_ENV !== 'test') {
-    await app.listen(process.env.PORT);
+    await app.listen(process.env.PORT, '0.0.0.0');
   }
 }
 bootstrap();
