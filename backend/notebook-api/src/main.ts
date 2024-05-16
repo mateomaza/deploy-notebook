24e49ab6a7a443bca5a447d692fc64fb5c66adc9
mateomaza <1mateomaza@gmail.com>
2024-05-16 08:27:43 -0500

    Correct Deploy

--- backend/notebook-api/src/main.ts
+++ backend/notebook-api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://deploy-notebook-4x8ljww9e-mateomazas-projects.vercel.app/',
+    origin: 'https://deploy-notebook.vercel.app/',
   });
   if (process.env.NODE_ENV !== 'test') {
     await app.listen(3001);
