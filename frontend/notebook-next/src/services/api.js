df14dcf8948d31283caf6d1e8e4388af906776e5
mateomaza <1mateomaza@gmail.com>
2024-05-16 12:33:35 -0500

    Literally just trying

--- frontend/notebook-next/src/services/api.js
+++ frontend/notebook-next/src/services/api.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://deploy-notebook-production.up.railway.app',
-  withCredentials: true
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
 });
 
 export default api;
