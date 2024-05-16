882a77cdf62b7f7c6366eae34d7a5f86abf19d3d
mateomaza <1mateomaza@gmail.com>
2024-05-16 16:07:38 -0500

    4.20

--- backend/notebook-api/src/main.ts
+++ backend/notebook-api/src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://deploy-notebook.vercel.app'],
+    credentials: true,
+  });
   if (process.env.NODE_ENV !== 'test') {
     await app.listen(process.env.PORT, '0.0.0.0');
   }
