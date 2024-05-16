da19e830126db376d47d910555dd0e6dd0f08efd
mateomaza <1mateomaza@gmail.com>
2024-05-16 10:51:30 -0500

    Last change I hope

--- frontend/notebook-next/src/services/api.js
+++ frontend/notebook-next/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://notebook-deploy-production.up.railway.app',
+  baseURL: 'https://deploy-notebook-production.up.railway.app',
   withCredentials: true
 });
 
