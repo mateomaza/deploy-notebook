a1925ee58019ee94429241f4572a8ca8a47a7aa4
mateomaza <1mateomaza@gmail.com>
2024-05-16 09:42:10 -0500

    One more

--- backend/notebook-api/src/main.ts
+++ backend/notebook-api/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: 'https://deploy-notebook.vercel.app',
+    credentials: true,
+  });
   if (process.env.NODE_ENV !== 'test') {
     await app.listen(3001);
   }
--- frontend/notebook-next/src/services/api.js
+++ frontend/notebook-next/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://deploy-notebook-production.up.railway.app'
+  baseURL: 'https://deploy-notebook-production.up.railway.app',
+  withCredentials: true
 });
 
 export default api;
