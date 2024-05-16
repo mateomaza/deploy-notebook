21e10e9fbf051a3ab9fc0a7046960decc96a4676
mateomaza <1mateomaza@gmail.com>
2024-05-16 09:14:54 -0500

    Cors for all origins

--- backend/notebook-api/src/main.ts
+++ backend/notebook-api/src/main.ts
@@ -1,14 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors({
-    origin: 'https://deploy-notebook.vercel.app',
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }));
+  app.enableCors();
   if (process.env.NODE_ENV !== 'test') {
     await app.listen(3001);
   }
