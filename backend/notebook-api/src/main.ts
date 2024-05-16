637ee365da5ab85b0c859c68e6f11c89f5db9056
mateomaza <1mateomaza@gmail.com>
2024-05-16 08:52:15 -0500

    Fix frontend url

--- backend/notebook-api/src/main.ts
+++ backend/notebook-api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://deploy-notebook.vercel.app/',
+    origin: 'https://deploy-notebook.vercel.app',
   });
   if (process.env.NODE_ENV !== 'test') {
     await app.listen(3001);
