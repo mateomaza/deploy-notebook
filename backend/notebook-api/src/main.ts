ff609057655d6b9c7b01b68c0472a5c59d6e9d37
mateomaza <1mateomaza@gmail.com>
2024-05-16 09:30:50 -0500

    Still trying

--- backend/notebook-api/src/main.ts
+++ backend/notebook-api/src/main.ts
@@ -2,12 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: 'https://deploy-notebook.vercel.app',
-    credentials: true,
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
   if (process.env.NODE_ENV !== 'test') {
     await app.listen(3001);
   }
